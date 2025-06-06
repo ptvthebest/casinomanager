4ea643b28afdb84f1bd6000e8d0c054aa83cc3db
ptvthebest <brajkovic.mia5555@gmail.com>
2025-06-06 05:54:17 +0200

    Update casino.h

--- casino.h
+++ casino.h
@@ -4,28 +4,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
+#define MAX_NAME_LENGTH 50
+#define ERROR_EXIT 1
+
+// Enum za tip igre
+typedef enum {
+    SLOT_MACHINE,
+    ROULETTE,
+    BLACKJACK,
+    POKER
+} GameType;
+
+// Union za dodatne podatke o okladi
+typedef union {
+    int spinNumber;
+    char dealerName[MAX_NAME_LENGTH];
+} BetData;
+
+// Struktura za igrača
 typedef struct {
     int playerID;
-    char firstName[50];
-    char lastName[50];
+    char firstName[MAX_NAME_LENGTH];
+    char lastName[MAX_NAME_LENGTH];
     double balance;
 } Player;
 
+// Struktura za igru
 typedef struct {
     int gameID;
-    char gameName[50];
+    char gameName[MAX_NAME_LENGTH];
     double minBet;
     double maxBet;
+    GameType type;
+    BetData extraData;
 } Game;
 
- Player players[100];
- Game games[100];
- int playerCount;
- int gameCount;
- int nextPlayerID;
- int nextGameID;
+// Enum za izbornik
+typedef enum {
+    DODAJ_IGRACA = 1,
+    PRIKAZI_IGRACE,
+    AZURIRAJ_IGRACA,
+    OBRISI_IGRACA,
+    DODAJ_IGRU,
+    PRIKAZI_IGRE,
+    AZURIRAJ_IGRU,
+    OBRISI_IGRU,
+    SORTIRAJ_IGRACE,
+    PRETRAZI_IGRACE,
+    PRINTAJ_REKURZIVNO,
+    SORTIRAJ_IGRE,
+    PREIMENUJ_DATOTEKU,
+    OBRISI_DATOTEKU,
+    ISPISI_MATRICU,
+    SORTIRAJ_IGRACE_FP,
+    IZLAZ
+} MenuOption;
 
+extern Player* players;
+extern Game* games;
+extern int playerCount;
+extern int gameCount;
+extern int nextPlayerID;
+extern int nextGameID;
+
+// Funkcije za igrače
 void addPlayer();
 void listPlayers();
 void updatePlayer();
@@ -34,6 +78,10 @@ void savePlayers();
 void loadPlayers();
 void sortPlayers();
 
+Player* findPlayerByID(int id);
+Player* searchPlayerBinary(int id);
+void recursivePrintPlayers(int index);
+
 void addGame();
 void listGames();
 void updateGame();
@@ -41,5 +89,17 @@ void deleteGame();
 void saveGames();
 void loadGames();
 void sortGames();
+
+void freeMemory();
+
+void renameFile(const char* oldName, const char* newName);
+void deleteFile(const char* filename);
+
+void printMatrix();
+
+typedef int (*CompareFunc)(const void*, const void*);
+void sortPlayersWithFunctionPointer(CompareFunc cmp);
+
+int compareByBalance(const void* a, const void* b);
 
 #endif

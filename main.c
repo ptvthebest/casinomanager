4d70e54328d713592d1d43f707b2719ea00b38aa
ptvthebest <brajkovic.mia5555@gmail.com>
2025-06-06 05:53:49 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,48 +1,106 @@
 #define _CRT_SECURE_NO_WARNINGS
-
 #include "casino.h"
 
 int main() {
     loadPlayers();
     loadGames();
 
-    int opcija;
+    int choice;
     do {
-        printf("\n--- Casino Manager ---\n");
+        printf("\n=== Casino Game Manager ===\n");
         printf("1. Dodaj igraca\n");
-        printf("2. Prikazi igrace\n");
+        printf("2. Prikazi sve igrace\n");
         printf("3. Azuriraj saldo igraca\n");
         printf("4. Obrisi igraca\n");
-        printf("5. Sortiraj igrace\n");
-        printf("6. Dodaj igru\n");
-        printf("7. Prikazi igre\n");
-        printf("8. Sortiraj igre\n");
+        printf("5. Dodaj igru\n");
+        printf("6. Prikazi sve igre\n");
+        printf("7. Azuriraj igru\n");
+        printf("8. Obrisi igru\n");
+        printf("9. Sortiraj igrace po saldu\n");
+        printf("10. Pretrazi igraca po ID-u (binarna pretraga)\n");
+        printf("11. Ispis igraca rekurzijom\n");
+        printf("12. Sortiraj igre (bubble sort)\n");
+        printf("13. Preimenuj datoteku\n");
+        printf("14. Obrisi datoteku\n");
+        
+        printf("16. Sortiraj igrace funkcijskim pokazivacem\n");
         printf("0. Izlaz\n");
         printf("Odabir: ");
-        scanf("%d", &opcija);
+        (void)scanf("%d", &choice);
+        (void)getchar();
 
-        switch (opcija) {
-        case 1: addPlayer();
+        switch (choice) {
+        case 1:
+            addPlayer();
             break;
-        case 2: listPlayers(); 
+        case 2:
+            listPlayers();
             break;
-        case 3: updatePlayer();
+        case 3:
+            updatePlayer();
             break;
-        case 4: deletePlayer(); 
+        case 4:
+            deletePlayer();
             break;
-        case 5: sortPlayers(); 
+        case 5:
+            addGame();
             break;
-        case 6: addGame(); 
+        case 6:
+            listGames();
             break;
-        case 7: listGames(); 
+        case 7:
+            updateGame();
             break;
-        case 8: sortGames();
+        case 8:
+            deleteGame();
             break;
-        case 0: printf("Izlaz...\n"); 
+        case 9:
+            sortPlayers();
             break;
-        default: printf("Nepoznata opcija.\n");
+        case 10: {
+            int id;
+            printf("Unesite ID igraca za pretragu: ");
+            (void)scanf("%d", &id);
+            Player* p = searchPlayerBinary(id);
+            if (p)
+                printf("Pronadjen: %s %s - Saldo: %.2lf\n", p->firstName, p->lastName, p->balance);
+            else
+                printf("Igrac nije pronadjen.\n");
+            break;
         }
-    } while (opcija != 0);
+        case 11:
+            recursivePrintPlayers(0);
+            break;
+        case 12:
+            sortGames();
+            break;
+        case 13: {
+            char oldName[100], newName[100];
+            printf("Unesite staro ime datoteke: ");
+            (void)scanf("%99s", oldName);
+            printf("Unesite novo ime datoteke: ");
+            (void)scanf("%99s", newName);
+            renameFile(oldName, newName);
+            break;
+        }
+        case 14: {
+            char filename[100];
+            printf("Unesite ime datoteke za brisanje: ");
+            (void)scanf("%99s", filename);
+            deleteFile(filename);
+            break;
+        }
+       
+        case 16:
+            sortPlayersWithFunctionPointer(compareByBalance);
+            break;
+        case 0:
+            break;
+        default:
+            printf("Nevazeci odabir.\n");
+        }
+    } while (choice != 0);
 
+    freeMemory();
     return 0;
 }
